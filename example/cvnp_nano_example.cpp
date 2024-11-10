19a816b24f1c9a12e1232f2c8e8d9c842d0cdaac
Pascal Thomet <pthomet@gmail.com>
2024-11-10 19:04:59 +0100

    initial commit

---
+++ example/cvnp_nano_example.cpp
@@ -0,0 +1,29 @@
+#include <nanobind/nanobind.h>
+//#include "cvnp/cvnp.h"
+
+//void pydef_cvnp(pybind11::module& m);
+//void pydef_cvnp_test(pybind11::module& m);
+
+
+int add(int i, int j) {
+    return i + j;
+}
+
+//#include <opencv2/opencv.hpp>
+//template <typename _Tp>
+//void inspect(const cv::Mat_<_Tp> mat)
+//{
+//    std::cout << "[C++] Inspect cv::Mat_<_Tp>" << std::endl;
+//    std::cout << "        rows: " << mat.rows << std::endl;
+//    std::cout << "        cols: " << mat.cols << std::endl;
+//    std::cout << "        channels: " << mat.channels() << std::endl;
+//    std::cout << "        type: " << cv::typeToString(mat.type()) << std::endl;
+//}
+
+
+NB_MODULE(cvnp_nano, m)
+{
+    m.def("add", &add);
+//    pydef_cvnp_test(m);
+//    pydef_cvnp(m);
+}
